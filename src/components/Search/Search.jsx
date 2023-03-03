6f16a293ad0b06ebb7e1dd0076f9e6f1d9ff6996
Arian Hosseini <arianhosseini@outlook.com>
2023-03-03 19:58:52 +0330

    feat: added <Search /> component

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -2,8 +2,11 @@ import styles from "./Search.module.css";
 
 const Search = () => {
 	return (
-		<div className={styles.Search}>
-			<div className="container">Search Component</div>
+		<div className="container">
+			<div className={styles.Search}>
+				<ion-icon name="search-outline"></ion-icon>
+				<input type="search" className={styles.SearchInput} placeholder="Search" />
+			</div>
 		</div>
 	);
 };
