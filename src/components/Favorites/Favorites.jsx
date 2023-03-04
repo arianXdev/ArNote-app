0475bfe40aa6770ae3d7a1c9e8507cff50444417
Arian Hosseini <arianhosseini@outlook.com>
2023-03-04 20:34:52 +0330

    feat: finished <Favorites /> component

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -1,10 +1,16 @@
+import { Note } from "..";
+
 import "./Favorites.css";
 
-const Favorites = () => {
+const Favorites = ({ favoriteNotes }) => {
 	return (
-		<section className="favorites hidden">
+		<section className={favoriteNotes.length > 0 ? "favorites" : "favorites hidden"}>
 			<h2 className="favorites__title">Favorites</h2>
-			<div className="favorites__body"></div>
+			<div className="favorites__body">
+				{favoriteNotes.map((note) => (
+					<Note key={note.id} note={note} />
+				))}
+			</div>
 		</section>
 	);
 };
