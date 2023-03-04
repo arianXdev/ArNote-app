41d2f5b7461f23b142f925c2e51f692b1f5d7874
Arian Hosseini <arianhosseini@outlook.com>
2023-03-04 21:24:42 +0330

    feat: added colors to notes using classNames library

--- src/components/Note/Note.jsx
+++ src/components/Note/Note.jsx
@@ -1,8 +1,20 @@
+import classNames from "classnames";
+
 import "./Note.css";
 
 const Note = ({ note: { isFavorite, id, category, body, date } }) => {
+	const noteClass = classNames({
+		note: true,
+		favorite: isFavorite,
+		blue: category === "1",
+		gray: category === "2",
+		green: category === "3",
+		red: category === "4",
+		yellow: category === "5",
+	});
+
 	return (
-		<article className={isFavorite ? "note favorite" : "note"} id={id} category={category}>
+		<article className={noteClass} id={id} category={category}>
 			<div className="note__body">{body}</div>
 			<div className="note__date">
 				{date.month} {date.day}, {date.year}
