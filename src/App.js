f3c0dff102704edc7f6d0d1aae74cbd79d69f890
Arian Hosseini <arianhosseini@outlook.com>
2023-03-04 21:06:28 +0330

    feat: GET all notes and categories from server

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
+import { useEffect } from "react";
+import { useImmer } from "use-immer";
+
 import { Search, Favorites, Notes } from "./components";
 
-import { useImmer } from "use-immer";
+import { getAllNotes, getAllCategories } from "./services/notesService";
 
 import "./App.css";
 
 const App = () => {
 	const [notes, setNotes] = useImmer([]);
 	const [favoriteNotes, setFavoriteNotes] = useImmer([]);
+	const [categories, setCategories] = useImmer([]);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				// Get all notes from server
+				const { data: notesData } = await getAllNotes();
+				// Get all categories from server
+				const { data: categoriesData } = await getAllCategories();
+
+				setFavoriteNotes(notesData.filter((note) => note.isFavorite === true));
+				setNotes(notesData.filter((note) => note.isFavorite === false));
+
+				setCategories(categoriesData);
+			} catch (err) {
+				console.log(err.message);
+			}
+		};
+
+		fetchData();
+	}, []);
 
 	return (
 		<div className="App">
