549a0a0399d24463f52259267b083c9c65496bff
Arian Hosseini <arianhosseini@outlook.com>
2023-03-04 20:12:50 +0330

    chore: added <BrowserRouter /> as a wrapper

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
+
+import { BrowserRouter } from "react-router-dom";
+
 import App from "./App";
 
 import "./index.css";
@@ -7,6 +10,8 @@ import "./index.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<StrictMode>
-		<App />
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
 	</StrictMode>
 );
